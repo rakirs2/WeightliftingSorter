abf2653112759406110c8662697ac0564732f811
rakirs2 <srmylavarapu@microsoft.com>
2022-08-23 20:24:01 -0500

    basic implementation ideas

--- Sorter.py
+++ Sorter.py
@@ -1,1 +1,11 @@
+import csv
+
 print("Hello Weightlifting")
+
+# open a csv file
+with open ('benches.csv') as csvfile:
+    # create a reader of the csv file, quotechar can be almost anything that isn't in the dataset
+    values = csv.reader(csvfile)
+    # for every row in the reader value
+    for row in values:
+        print( row)
