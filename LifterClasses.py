65d0e5df3cce21ad69f70ff5f4f52638db668675
rakirs2 <srmylavarapu@microsoft.com>
2022-10-01 13:43:41 -0500

    updating files, need to add print csv func

--- LifterClasses.py
+++ LifterClasses.py
@@ -1,30 +1,28 @@
-class PowerLiftingMeetResult:
-    def __init__(self, playerId, Name, Sex, Equipment, Age, BodyweightKg, BestSquatKg, BestDeadliftKg):
-        self.playerId = playerId
-        self.Name = Name
-        self.Sex = Sex
-        self.Equipment = Equipment
-        self.BodyweightKg = BodyweightKg
-        self.BestSquatKg = BestSquatKg
-        self.BestDeadliftKg = BestDeadliftKg
+from functools import singledispatch
+
 
 class WeightLiftingMeetResult:
-    def __init__(self, meet, date,):
+    def __init__(self, meet, date, category, name, bodyweight, snatch1, snatch2, snatch3, clean1, clean2, clean3):
         self.meet = meet
         self.date = date
-        #  age, category, name, weight, snatch1, snatch2, snatch3,clean1, clean2, clean3
-        # self.age = int(age)
-        # self.category = category
-        # self.name = name
-        # self.bodyweight = int(weight)
-        # self.snatch1 = int(snatch1)
-        # self.snatch2 = int(snatch2)
-        # self.snatch3 = int(snatch3)
-        # self.maxSnatch = max([0, snatch1, snatch2, snatch3])
-        # self.clean1 = int(clean1)
-        # self.clean2 = int(clean2)
-        # self.clean3 = int(clean3)
-        # self.maxClean = max([0, clean1, clean2, clean3])
-        # self.total = self.maxSnatch+ self.maxClean
+        self.category = category
+        self.name = name
+        self.bodyweight = bodyweight
+
+        self.snatch1 = convertToInt(snatch1)
+        self.snatch2 = convertToInt(snatch2)
+        self.snatch3 = convertToInt(snatch3)
+        self.maxSnatch = max(0, self.snatch1, self.snatch2, self.snatch3)
+        self.clean1 = convertToInt(clean1)
+        self.clean2 = convertToInt(clean2)
+        self.clean3 = convertToInt(clean3)
+        self.maxClean = max(0, self.clean1, self.clean2, self.clean3)
+        self.total = self.maxSnatch+ self.maxClean
     def __str__(self):
-        return self.meet + ", " + self.date
+        return self.meet + ", " + self.date+", " + self.category + ", " + self.category + ", " + self.bodyweight + ", " + str(self.maxSnatch)+", " +str(self.maxClean) + ", " +str(self.total)
+
+def convertToInt(val):
+    try:
+        return int(val)
+    except:
+        return 0
